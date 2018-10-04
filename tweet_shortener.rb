b77d1290eff5d3af02859f9082dceb99cc635ba1
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 20:49:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,4 +29,10 @@ def bulk_tweet_shortener(array_of_tweets)
   end
 end
 
-def 
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
